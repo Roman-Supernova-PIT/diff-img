44bdc7c2b9073ea2ef8c6074d3d6ba3ace61ed2a
Lauren Aldoroty <laurenaldoroty@gmail.com>
2024-08-02 10:43:50 -0400

    change environment activated in sh files to correct ones

--- mk_lc.sh
+++ mk_lc.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name=lc
 #SBATCH --mem=8G
 #SBATCH --output /hpc/group/cosmology/lna18/rsim_photometry/dcc_output/lc/lc-%j.out
-#SBATCH --partition=cosmology
+##SBATCH --partition=cosmology
 #SBATCH --mail-user=lauren.aldoroty@duke.edu
 #SBATCH --mail-type=ALL
 #SBATCH --array=5
 
 # Activate conda environment
 source ~/.bashrc
-conda activate repeatability
+conda activate diff
 
 python -u mk_lc.py 20172782 1 --inputdir /work/lna18/ --outputdir /hpc/group/cosmology/lna18/roman_sim_imgs/ --slurm_array
