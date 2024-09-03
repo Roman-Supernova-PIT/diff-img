a533f61d9ec90f36b27f8dba5e348ba439c57c21
Lauren Aldoroty <laurenaldoroty@gmail.com>
2024-09-03 10:09:33 -0700

    so i can switch to main

--- sfft_and_animate_nersc.sh
+++ sfft_and_animate_nersc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -A m4385
-#SBATCH -C cpu
+#SBATCH -C gpu
 #SBATCH -q debug # regular or debug
 #SBATCH --job-name=sfft
 ##SBATCH --mem=64G # Comment this out while on -q debug because you get a whole node, so you can monitor with top to see how much you're using. 
@@ -10,11 +10,11 @@
 #SBATCH --output=/global/cfs/cdirs/m4385/users/lauren/out_logs/sfft/sfft-%J.out
 #SBATCH --mail-user=lauren.aldoroty@duke.edu
 #SBATCH --mail-type=ALL
-##SBATCH --gpus-per-task 1
-#SBATCH --cpus-per-task 4
+#SBATCH --gpus-per-task 1
+#SBATCH --cpus-per-task 1
 ##SBATCH --time=2:00:00 # regular QOS
 #SBATCH --time=30:00 # debug QOS
-#SBATCH --array=3
+#SBATCH --array=5
 
 # Activate conda environment
 source ~/.bashrc
@@ -40,22 +40,25 @@ export LC_OUT_DIR="/global/cfs/cdirs/m4385/users/lauren/lcs" # Parent folder of 
 # Run program. 
 sne=( '20172782' )
 # sne=( '20172117' '20172782' '20173305' '20174023' '20174118' '20174370' '20175077' '20177380' '20172328' '20173301' '20173373' '20174108' '20174213' '20174542' '20175568' '20202893' )
+# sne=( '20175077' '20177380' '20172328' '20173301' '20173373' '20174108' '20174213' '20174542' '20175568' '20202893' )
 
 for sn in "${sne[@]}"
 do
     echo "$sn"
     # Step 1: Get all images the object is in.
-    # python -u get_object_instances.py "$sn"
+    # python -u get_object_instances.py "$sn" # We actually only want to do this step once per object.
+                                            # There is no reason to do it once per job array
+                                            # element (i.e., once per object per band).
     # Step 2: Sky subtract, align images to be in DIA. 
     # WAIT FOR COMPLETION. 
     # Step 3: Get, align, save PSFs; cross-convolve. 
-    # srun python -u preprocess.py "$sn" --n-templates 1 --verbose True --slurm_array
+    srun python -u preprocess.py "$sn" --n-templates 1 --verbose True --slurm_array
     # WAIT FOR COMPLETION.
     # Step 4: Differencing (GPU). 
-    # srun python -u sfftdiff.py "$sn" --n-templates 1 --verbose True --slurm_array
+    srun python -u sfftdiff.py "$sn" --n-templates 1 --verbose True --slurm_array
     # WAIT FOR COMPLETION.
     # Step 5: Generate decorrelation kernel, apply to diff. image and science image, make stamps. 
-    # srun python -u postprocess.py "$sn" --n-templates 1 --verbose True --slurm_array
+    srun python -u postprocess.py "$sn" --n-templates 1 --verbose True --slurm_array
     # WAIT FOR COMPLETION.
     # Step 6: Make LC and generate plots.
     python -u mk_lc.py "$sn" --n-templates 1 --slurm_array
