74c72c573316c029d11b97bc3ce443b57993551b
Rob Knop <raknop@lbl.gov>
2024-09-09 14:43:09 -0700

    hacks to make it work

--- sfft_and_animate_se.py
+++ sfft_and_animate_se.py
@@ -1,9 +1,11 @@
+import os
 from phrosty.imagesubtraction import imsub
 
-refpath = '/cwork/mat90/RomanDESC_sims_2024/RomanTDS/images/simple_model/R062/6/Roman_TDS_simple_model_R062_6_17.fits.gz'
-scipath = '/cwork/mat90/RomanDESC_sims_2024/RomanTDS/images/simple_model/R062/35083/Roman_TDS_simple_model_R062_35083_8.fits.gz'
+refpath = os.path.join( os.getenv('SIMS_DIR'), 'Roman_TDS_simple_model_R062_6_17.fits.gz' )
+scipath = os.path.join( os.getenv('SIMS_DIR'), 'Roman_TDS_simple_model_R062_35083_8.fits.gz' )
 
-s = imsub(20172782,'R062','/work/lna18/imsub_out')
+s = imsub(20172782,'R062',os.getenv('DIA_OUT_DIR'))
+
 s.set_template(refpath)
 s.set_science(scipath)
 s.preprocess_template()
