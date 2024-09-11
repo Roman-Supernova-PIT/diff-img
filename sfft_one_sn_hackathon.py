ac1c5cc8593a54e63c979bfa60c114de7a8aad7e
Rob Knop <ns-6xf4yf@curiosity-headnode.cm.cluster>
2024-09-11 13:15:16 -0700

    temporary comment out nvxt in sfft_one_sn_hackathon.py

--- sfft_one_sn_hackathon.py
+++ sfft_one_sn_hackathon.py
@@ -1,4 +1,4 @@
-import nvtx
+# import nvtx
 
 import preprocess
 import sfftdiff
@@ -10,15 +10,14 @@ bands = [ 'R062' ]
 
 for sn in sne:
     for band in bands:
-        with nvtx.annotate( "preprocess", color="red" ):
-            preprocess.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
+        # with nvtx.annotate( "preprocess", color="red" ):
+        preprocess.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
 
-        with nvtx.annotate( "sfftdiff", color="green" ):
-            sfftdiff.run( sn, band, n_templates=1, verbose=True )
+        # with nvtx.annotate( "sfftdiff", color="green" ):
+        sfftdiff.run( sn, band, n_templates=1, verbose=True )
 
-        with nvtx.annotate( "postprocess", color="blue" ):
-            postprocess.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
+        # with nvtx.annotate( "postprocess", color="blue" ):
+        postprocess.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
 
-        with nvtx.annotate( "mk_lc", color="violet" ):
-            mk_lc.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
-            
+        # with nvtx.annotate( "mk_lc", color="violet" ):
+        mk_lc.run( sn, band, n_templates=1, verbose=True, cpus_per_task=1 )
